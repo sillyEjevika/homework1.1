4b936f1537614b27918630862f11fc0fccc41660
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-03 20:36:05 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       var dog = 8;
-       dog = dog + 4;
-        System.out.println(dog);
-        var cat = 3.6;
-        cat = cat + 4;
+       var dog = 8 - 3.5;
+       System.out.println(dog);
+        var cat = 3.6 - 1.6;
         System.out.println(cat);
-        var paper = 763789;
-        paper = paper + 4;
+        var paper = 763789 - 7639;
         System.out.println(paper);
     }
 }
